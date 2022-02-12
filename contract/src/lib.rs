ed30062ac5d12d2c55b5ab11bc887ab4246b5074
rotisant <roti.sant007@gmail.com>
2022-02-12 20:44:47 +0530

    initial

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,25 +1,9 @@
-/*
- * This is an example of a Rust smart contract with two simple, symmetric functions:
- *
- * 1. set_greeting: accepts a greeting, such as "howdy", and records it for the user (account_id)
- *    who sent the request
- * 2. get_greeting: accepts an account_id and returns the greeting saved for it, defaulting to
- *    "Hello"
- *
- * Learn more about writing NEAR smart contracts with Rust:
- * https://github.com/near/near-sdk-rs
- *
- */
-
-// To conserve gas, efficient serialization is achieved through Borsh (http://borsh.io/)
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, near_bindgen, setup_alloc};
 use near_sdk::collections::LookupMap;
+use near_sdk::{env, near_bindgen, setup_alloc};
 
 setup_alloc!();
 
-// Structs in Rust are similar to other languages, and may include impl keyword as shown below
-// Note: the names of the structs are not important when calling the smart contract, but the function names are
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Welcome {
@@ -27,11 +11,11 @@ pub struct Welcome {
 }
 
 impl Default for Welcome {
-  fn default() -> Self {
-    Self {
-      records: LookupMap::new(b"a".to_vec()),
+    fn default() -> Self {
+        Self {
+            records: LookupMap::new(b"a".to_vec()),
+        }
     }
-  }
 }
 
 #[near_bindgen]
---
+++ contract/src/utils.rs
@@ -0,0 +1,14 @@
+// Account
+pub const NEAR_MAIN_ACCOUNT: &str = "roti-sant.testnet";
+
+// Pools
+pub const DAI_ETH_POOL_ID: u32 = 193;
+pub const REF_ETH_POOL_ID: u32 = 321;
+pub const DAI_REF_POOL_ID: u32 = 326;
+
+// Contracts
+pub const REF_FARMING_CONTRACT: &str = "farm110.ref-dev.testnet";
+pub const REF_EXCHANGE_CONTRACT: &str = "exchange.ref-dev.testnet";
+pub const REF_TOKEN_CONTRACT: &str = "ref.fakes.testnet";
+pub const DAI_TOKEN_CONTRACT: &str = "dai.fakes.testnet";
+pub const ETH_TOKEN_CONTRACT: &str = "eth.fakes.testnet";
