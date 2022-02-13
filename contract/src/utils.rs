a0f0db3514a0d0831e8100a58b211bd729aef723
rotisant <roti.sant007@gmail.com>
2022-02-13 19:57:52 +0530

    change in Farming constants

--- contract/src/utils.rs
+++ contract/src/utils.rs
@@ -1,16 +1,21 @@
-use near_sdk::env;
-
-// Pools
-pub const DAI_ETH_POOL_ID: u32 = 193;
-pub const REF_ETH_POOL_ID: u32 = 321;
-pub const DAI_REF_POOL_ID: u32 = 326;
+use near_sdk::{env, ext_contract};
 
 // Contracts
-pub const REF_FARMING_CONTRACT: &str = "farm110.ref-dev.testnet";
-pub const REF_EXCHANGE_CONTRACT: &str = "exchange.ref-dev.testnet";
-pub const REF_TOKEN_CONTRACT: &str = "ref.fakes.testnet";
-pub const DAI_TOKEN_CONTRACT: &str = "dai.fakes.testnet";
-pub const ETH_TOKEN_CONTRACT: &str = "eth.fakes.testnet";
+pub const TOKEN1_CONTRACT_ID: &str = "paras.fakes.testnet";
+pub const TOKEN2_CONTRACT_ID: &str = "ref.fakes.testnet";
+pub const REWARDS_CONTRACT_IDS: (&str, &str) = ("ref.fakes.testnet", "pulse.fakes.testnet");
+pub const REF_FARMING_CONTRACT_ID: &str = "farm110.ref-dev.testnet";
+pub const REF_EXCHANGE_CONTRACT_ID: &str = "exchange.ref-dev.testnet";
+
+// Pools ( -1 means no pool exist)
+pub const LIQUIDITY_POOL_ID: u32 = 5;
+pub const REWARDS_TOKEN1_SWAP_POOLS_ID: (&i32, &i32) = (&5, &6);
+pub const REWARDS_TOKEN2_SWAP_POOLS_ID: (&i32, &i32) = (&-1, &290);
+
+#[ext_contract(ext_ref_farming_contract)]
+trait RefFarmingContract {
+    fn claim_reward_by_seed(&mut self, seed_id: String);
+}
 
 // Claim Rewards
 fn get_all_staked_seeds() -> Vec<String> {
@@ -32,6 +37,13 @@ fn claim_using_seeds(seed_id: String) -> bool {
 
 pub fn claim_rewards() -> bool {
     let mut harvesting_status = true;
+
+    ext_ref_farming_contract::claim_reward_by_seed(
+        "exchange.ref-dev.testnet@5".to_string(),
+        &REF_FARMING_CONTRACT_ID,
+        0,
+        100_000_000_000_000,
+    );
 
     let staked_seeds: Vec<String> = get_all_staked_seeds();
     if staked_seeds.len() == 0 {
