fd4c382eafd0264dbe82e96487d955d97e6060a1
rotisant <roti.sant007@gmail.com>
2022-02-20 17:13:15 +0530

    Change in format

--- src/App.js
+++ src/App.js
@@ -41,19 +41,10 @@ export default function App() {
   if (!window.walletConnection.isSignedIn()) {
     return (
       <main>
-        <h1>Welcome to NEAR!</h1>
+        <h1>Welcome to XPANSE!</h1>
         <p>
           To make use of the NEAR blockchain, you need to sign in. The button
           below will sign you in using NEAR Wallet.
-        </p>
-        <p>
-          By default, when your app runs in "development" mode, it connects
-          to a test network ("testnet") wallet. This works just like the main
-          network ("mainnet") wallet, but the NEAR Tokens on testnet aren't
-          convertible to other currencies – they're just for testing!
-        </p>
-        <p>
-          Go ahead and click the button below to try it out:
         </p>
         <p style={{ textAlign: 'center', marginTop: '2.5em' }}>
           <button onClick={login}>Sign in</button>
@@ -153,21 +144,20 @@ export default function App() {
           </fieldset>
         </form>
         <p>
-          Look at that! A Hello World app! This greeting is stored on the NEAR blockchain. Check it out:
+          The Xpanse can manage / execute an end-to-end auto-compounding strategy on Ref Finance.
         </p>
         <ol>
           <li>
-            Look in <code>src/App.js</code> and <code>src/utils.js</code> – you'll see <code>get_greeting</code> and <code>set_greeting</code> being called on <code>contract</code>. What's this?
+            Deposit your Seeds(LP Tokens) into Xpanse Contract.
           </li>
           <li>
-            Ultimately, this <code>contract</code> code is defined in <code>assembly/main.ts</code> – this is the source code for your <a target="_blank" rel="noreferrer" href="https://docs.near.org/docs/develop/contracts/overview">smart contract</a>.</li>
+            The Contract will execute auto-compounding strategy including staking of seeds, claiming rewards, swapping tokens and add Liquidity.
+          </li>
           <li>
-            When you run <code>yarn dev</code>, the code in <code>assembly/main.ts</code> gets deployed to the NEAR testnet. You can see how this happens by looking in <code>package.json</code> at the <code>scripts</code> section to find the <code>dev</code> command.</li>
+            Withdraw Your Seeds Whenever required.
+          </li>
         </ol>
         <hr />
-        <p>
-          To keep learning, check out <a target="_blank" rel="noreferrer" href="https://docs.near.org">the NEAR docs</a> or look through some <a target="_blank" rel="noreferrer" href="https://examples.near.org">example apps</a>.
-        </p>
       </main>
       {showNotification && <Notification />}
     </>
